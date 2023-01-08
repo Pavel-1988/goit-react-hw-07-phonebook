56debf743a93ffd719f46eeed6cc35720d3c6bcf
Pavel-1988 <102318642+Pavel-1988@users.noreply.github.com>
2023-01-08 12:54:49 +0200

    q

--- src/redux/contactsApi.js
+++ src/redux/contactsApi.js
@@ -2,4 +2,20 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const contactsApi = createApi({
   reducerPath: 'contactsApi',
+  tagTypes: ['contact'],
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://63b67deb58084a7af3b3c24c.mockapi.io/contacts/'
+  }),
+  endpoints: builder => ({
+    fetchContacts: builder.query({
+      query: () => 'contacts',
+      providesTags: ['Contact']
+    }),
+    createContact: builder.mutation({
+      query: body => ({
+        url: 'contacts',
+        method: 'POST'
+      })
+    })
+  })
 })
